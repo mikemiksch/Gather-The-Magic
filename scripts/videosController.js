175d5d1487f75013d0381fcfeab568cabbc7ba72
David <davidporter33@icloud.com>
2016-12-22 13:39:55 -0800

    fixed the issue with the videos not loading.

--- scripts/videosController.js
+++ scripts/videosController.js
@@ -5,6 +5,8 @@
     $('#search-again').hide();
     $('.card-data').remove();
     $('#videos').fadeIn();
+    $('.slider-nav').show();
+    $('slider-for').show();
   };
   module.videosController = videosController;
 }(window));
